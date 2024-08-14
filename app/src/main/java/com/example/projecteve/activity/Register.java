46975464655471016174b40ecfca14d60a32863b
Douglas Vinicius Dierings <2022322@student.cct.ie>
2024-08-14 10:51:46 +0100

    Started the navigation bar

--- app/src/main/java/com/example/projecteve/activity/Register.java
+++ app/src/main/java/com/example/projecteve/activity/Register.java
@@ -7,6 +7,7 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.CheckBox;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
@@ -18,6 +19,7 @@ import androidx.core.view.WindowInsetsCompat;
 
 import com.example.projecteve.MainActivity;
 import com.example.projecteve.R;
+import com.example.projecteve.models.UserModel;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
@@ -34,6 +36,7 @@ public class Register extends AppCompatActivity {
     private EditText edt_confirmation_password_register;
     private Button btn_log_in;
     private Button btn_login_register;
+    private ImageView back_button;
 
     private FirebaseAuth mAuth;
 
@@ -48,10 +51,14 @@ public class Register extends AppCompatActivity {
             return insets;
         });
 
+
+
+
         mAuth = FirebaseAuth.getInstance();
 
         btn_log_in = findViewById(R.id.btn_log_in);
         btn_login_register = findViewById(R.id.btn_login_register);
+        back_button = findViewById(R.id.back_button);
 
         edt_firstName_register = findViewById(R.id.edt_firstName_register);
         edt_lastName_register = findViewById(R.id.edt_lastName_register);
@@ -60,23 +67,51 @@ public class Register extends AppCompatActivity {
         edt_password_register = findViewById(R.id.edt_password_register);
         edt_confirmation_password_register = findViewById(R.id.edt_confirmation_password_register);
 
+
+        back_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Register.this, Login.class);
+                startActivity(intent);
+                finish();
+            }
+        });
+
+        btn_log_in.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Register.this, Login.class);
+                startActivity(intent);
+                finish();
+            }
+        });
+
+
+
+
         btn_login_register.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                String firstName = edt_firstName_register.getText().toString();
-                String lastName = edt_lastName_register.getText().toString();
-                String email = edt_email_register.getText().toString();
-                String employeeNumber = edt_employee_number.getText().toString();
+
+                UserModel userModel = new UserModel();
+
+                userModel.setFirstName(edt_firstName_register.getText().toString());
+                userModel.setLastName(edt_lastName_register.getText().toString());
+                userModel.setEmail(edt_email_register.getText().toString());
+                userModel.setEmployeeNumber(edt_employee_number.getText().toString());
                 String password = edt_password_register.getText().toString();
                 String passwordConfirmation = edt_confirmation_password_register.getText().toString();
 
 
-                if (!TextUtils.isEmpty(firstName) && !TextUtils.isEmpty(lastName) && !TextUtils.isEmpty(email) && !TextUtils.isEmpty(employeeNumber) && !TextUtils.isEmpty(password) && !TextUtils.isEmpty(passwordConfirmation)) {
+                if (!TextUtils.isEmpty(userModel.getFirstName()) && !TextUtils.isEmpty(userModel.getLastName()) && !TextUtils.isEmpty(userModel.getEmail()) && !TextUtils.isEmpty(userModel.getEmployeeNumber()) && !TextUtils.isEmpty(password) && !TextUtils.isEmpty(passwordConfirmation)) {
                     if (password.equals(passwordConfirmation)) {
-                        mAuth.createUserWithEmailAndPassword(email, password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                        mAuth.createUserWithEmailAndPassword(userModel.getEmail(), password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
                             @Override
                             public void onComplete(@NonNull Task<AuthResult> task) {
                                 if (task.isSuccessful()) {
+                                    userModel.setId(mAuth.getUid());
+                                    userModel.save();
+                                    Toast.makeText(Register.this, "Register", Toast.LENGTH_SHORT).show();
                                     openMainScreen();
                                 } else {
                                     String error = task.getException().getMessage();
---
+++ app/src/main/java/com/example/projecteve/models/UserModel.java
@@ -0,0 +1,67 @@
+package com.example.projecteve.models;
+
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.Exclude;
+import com.google.firebase.database.FirebaseDatabase;
+
+public class UserModel {
+
+    private String id;
+    private String firstName;
+    private String lastName;
+    private String EmployeeNumber;
+    private String email;
+
+    public UserModel() {
+    }
+
+    public UserModel(String id, String firstName, String lastName, String email) {
+        this.id = id;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+    }
+
+    @Exclude
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getEmployeeNumber() {return EmployeeNumber;}
+
+    public void setEmployeeNumber(String employeeNumber) {EmployeeNumber = employeeNumber;}
+
+    public void save(){
+        DatabaseReference reference = FirebaseDatabase.getInstance().getReference();
+        reference.child("users").child(getId()).setValue(this);
+    }
+
+}
