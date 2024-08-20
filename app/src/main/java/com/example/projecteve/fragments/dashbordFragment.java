76e6e548fcaddcb9edd47497e04add0e8b115f33
Douglas Vinicius Dierings <126350947+DouglasDierings@users.noreply.github.com>
2024-08-20 22:22:14 +0100

    user name is on the dashboard now

--- app/src/main/java/com/example/projecteve/fragments/dashbordFragment.java
+++ app/src/main/java/com/example/projecteve/fragments/dashbordFragment.java
@@ -2,28 +2,66 @@ package com.example.projecteve.fragments;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import com.example.projecteve.R;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link dashbordFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
+
 public class dashbordFragment extends Fragment {
 
     View view;
+    FirebaseUser currentUser;
+    DatabaseReference databaseReference;
+    private FirebaseAuth mAuth;
+    private TextView txt_name;
+    private String uid;
+    ValueEventListener valueEventListener;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         view = inflater.inflate(R.layout.fragment_dashbord, container, false);
+
+        txt_name = view.findViewById(R.id.txt_name);
+
+        currentUser = FirebaseAuth.getInstance().getCurrentUser();
+
+        mAuth = FirebaseAuth.getInstance();
+
+        uid = currentUser.getUid();
+        databaseReference = FirebaseDatabase.getInstance().getReference().child("users").child(uid);
+        valueEventListener = new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                String firstName = snapshot.child("firstName").getValue(String.class);
+                String lastName = snapshot.child("lastName").getValue(String.class);
+                String fullName = firstName + " " + lastName;
+
+                txt_name.setText(fullName);
+
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+                Toast.makeText(dashbordFragment.this.getActivity(), "User not found", Toast.LENGTH_SHORT).show();
+            }
+        };
+        databaseReference.addValueEventListener(valueEventListener);
         return view;
     }
 }
---
+++ app/src/main/java/com/example/projecteve/models/Course.java
@@ -0,0 +1,29 @@
+package com.example.projecteve.models;
+
+public class Course {
+    private String courseName;
+    private Boolean isCompleted;
+
+    public Course() {}
+
+    public Course(String courseName, Boolean isCompleted) {
+        this.courseName = courseName;
+        this.isCompleted = isCompleted;
+    }
+
+    public String getCourseName() {
+        return courseName;
+    }
+
+    public void setCourseName(String courseName) {
+        this.courseName = courseName;
+    }
+
+    public Boolean getIsCompleted() {
+        return isCompleted;
+    }
+
+    public void setIsCompleted(Boolean isCompleted) {
+        this.isCompleted = isCompleted;
+    }
+}
