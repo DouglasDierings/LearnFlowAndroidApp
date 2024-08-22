5975bfba8ade4633697a12ccb3cc319fdbf9070c
Douglas Vinicius Dierings <126350947+DouglasDierings@users.noreply.github.com>
2024-08-22 17:40:40 +0100

    Fixed bug that dashboard was pointing to null user

--- app/src/main/java/com/example/projecteve/fragments/dashbordFragment.java
+++ app/src/main/java/com/example/projecteve/fragments/dashbordFragment.java
@@ -1,5 +1,6 @@
 package com.example.projecteve.fragments;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
@@ -12,6 +13,7 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 import com.example.projecteve.R;
+import com.example.projecteve.activity.login;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DataSnapshot;
@@ -38,30 +40,33 @@ public class dashbordFragment extends Fragment {
         view = inflater.inflate(R.layout.fragment_dashbord, container, false);
 
         txt_name = view.findViewById(R.id.txt_name);
-
-        currentUser = FirebaseAuth.getInstance().getCurrentUser();
-
         mAuth = FirebaseAuth.getInstance();
+        currentUser = mAuth.getCurrentUser();
 
-        uid = currentUser.getUid();
-        databaseReference = FirebaseDatabase.getInstance().getReference().child("users").child(uid);
-        valueEventListener = new ValueEventListener() {
-            @Override
-            public void onDataChange(@NonNull DataSnapshot snapshot) {
-                String firstName = snapshot.child("firstName").getValue(String.class);
-                String lastName = snapshot.child("lastName").getValue(String.class);
-                String fullName = firstName + " " + lastName;
+        if (currentUser != null) {
+            uid = currentUser.getUid();
+            databaseReference = FirebaseDatabase.getInstance().getReference().child("users").child(uid);
+            valueEventListener = new ValueEventListener() {
+                @Override
+                public void onDataChange(@NonNull DataSnapshot snapshot) {
+                    String firstName = snapshot.child("firstName").getValue(String.class);
+                    String lastName = snapshot.child("lastName").getValue(String.class);
+                    String fullName = firstName + " " + lastName;
 
-                txt_name.setText(fullName);
+                    txt_name.setText(fullName);
+                }
 
-            }
+                @Override
+                public void onCancelled(@NonNull DatabaseError error) {
+                    Toast.makeText(dashbordFragment.this.getActivity(), "User not found", Toast.LENGTH_SHORT).show();
+                }
+            };
+            databaseReference.addValueEventListener(valueEventListener);
+        } else {
+            startActivity(new Intent(getActivity(), login.class));
+            getActivity().finish(); // Optional: finish the current activity
+        }
 
-            @Override
-            public void onCancelled(@NonNull DatabaseError error) {
-                Toast.makeText(dashbordFragment.this.getActivity(), "User not found", Toast.LENGTH_SHORT).show();
-            }
-        };
-        databaseReference.addValueEventListener(valueEventListener);
         return view;
     }
 }
