68fec7865e680cf8dcfdd9123f217f8d741ea9ff
Douglas Vinicius Dierings <126350947+DouglasDierings@users.noreply.github.com>
2024-09-13 18:45:46 +0100

    Refactor code with Lambda functions

--- app/src/main/java/com/example/projecteve/activity/Login.java
+++ app/src/main/java/com/example/projecteve/activity/Login.java
@@ -10,14 +10,10 @@ import android.widget.ProgressBar;
 import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
-import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.projecteve.MainActivity;
 import com.example.projecteve.R;
-import com.google.android.gms.tasks.OnCompleteListener;
-import com.google.android.gms.tasks.Task;
-import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 
 import java.util.Objects;
@@ -25,71 +21,52 @@ import java.util.Objects;
 public class Login extends AppCompatActivity {
 
 
-    //Buttons
-    private EditText edt_email;
-    private EditText edt_password;
-    private Button btn_sign_in_button;
-    private Button btn_register;
-    private Button btn_forgot_password;
-    private FirebaseAuth mAuth;
-    private ProgressBar loginProgressBar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        FirebaseAuth mAuth;
+        ProgressBar loginProgressBar;
+        EditText edtPassword;
+        EditText edtEmail;
+        Button btnRegister;
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_login);
 
 
         mAuth = FirebaseAuth.getInstance();
-        edt_email = findViewById(R.id.edt_email);
-        edt_password = findViewById(R.id.edt_password);
-        btn_sign_in_button = findViewById(R.id.btn_sign_in_button);
+        edtEmail = findViewById(R.id.edt_email);
+        edtPassword = findViewById(R.id.edt_password);
+        Button btnSignInButton = findViewById(R.id.btn_sign_in_button);
         loginProgressBar = findViewById(R.id.loginProgressBar);
-        btn_register = findViewById(R.id.btn_register);
-        btn_forgot_password = findViewById(R.id.btn_forgot_password);
+        btnRegister = findViewById(R.id.btn_register);
+        Button btnForgotPassword = findViewById(R.id.btn_forgot_password);
 
         //Do the Login
 
-        btn_sign_in_button.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                String loginEmail = edt_email.getText().toString();
-                String loginPassword = edt_password.getText().toString();
+        btnSignInButton.setOnClickListener(v -> {
+            String loginEmail = edtEmail.getText().toString();
+            String loginPassword = edtPassword.getText().toString();
 
-                if (!TextUtils.isEmpty(loginEmail) && !TextUtils.isEmpty(loginPassword)) {
-                    loginProgressBar.setVisibility(View.VISIBLE);
-                    mAuth.signInWithEmailAndPassword(loginEmail, loginPassword)
-                            .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
-                                @Override
-                                public void onComplete(@NonNull Task<AuthResult> task) {
-                                    if (task.isSuccessful()) {
-                                        openMainScreen();
-                                    } else {
-                                        String error = Objects.requireNonNull(task.getException()).getMessage();
-                                        Toast.makeText(Login.this, "" + error, Toast.LENGTH_SHORT).show();
-                                        loginProgressBar.setVisibility(View.INVISIBLE);
-                                    }
-                                }
-                            });
-                }
-
+            if (!TextUtils.isEmpty(loginEmail) && !TextUtils.isEmpty(loginPassword)) {
+                loginProgressBar.setVisibility(View.VISIBLE);
+                mAuth.signInWithEmailAndPassword(loginEmail, loginPassword)
+                        .addOnCompleteListener(task -> {
+                            if (task.isSuccessful()) {
+                                openMainScreen();
+                            } else {
+                                String error = Objects.requireNonNull(task.getException()).getMessage();
+                                Toast.makeText(Login.this, "" + error, Toast.LENGTH_SHORT).show();
+                                loginProgressBar.setVisibility(View.INVISIBLE);
+                            }
+                        });
             }
+
         });
 
-        btn_register.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                openRegisterScreen();
-            }
-        });
+        btnRegister.setOnClickListener(v -> openRegisterScreen());
 
-        btn_forgot_password.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                openRecoveryScreen();
-            }
-        });
+        btnForgotPassword.setOnClickListener(v -> openRecoveryScreen());
 
     }
 
